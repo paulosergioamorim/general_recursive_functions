7c474d474e85b447766620fdee1379d5954c0033
Paulo Sérgio <pamorimmonico@gmail.com>
2026-08-03 14:38:02 -0300

    enhance and new print style

--- main.py
+++ main.py
@@ -7,45 +7,61 @@ class PrimitiveRecursive:
         kargs = kargs
         raise NotImplementedError()
 
+
+"""
+Basic Primitive Recursive Functions Set
+"""
+
+
 class Successor(PrimitiveRecursive):
     def __call__(self, x: int) -> int:
-        return x+1
+        assert x >= 0
+        return x + 1
 
     def __str__(self) -> str:
-        return "(x -> x+1)"
+        return "S"
 
-class Zero(PrimitiveRecursive):
-    def __call__(self, _: int) -> int:
-        return 0
 
-    def __str__(self) -> str:
-        return "(x -> 0)"
+class Constant(PrimitiveRecursive):
+    def __init__(self, n: int, k: int):
+        assert n >= 0
+        assert k >= 0
+        self.ninputs = n
+        self.k = k
 
-class ZeroNoArgs(PrimitiveRecursive):
-    def __init__(self):
-        self.ninputs = 0
-
-    def __call__(self) -> int:
-        return 0
+    def __call__(self, *xs: int) -> int:
+        assert len(xs) == self.ninputs
+        return self.k
 
     def __str__(self) -> str:
-        return "0"
+        return f"C^{self.ninputs}_{self.k}"
+
 
 class Proj(PrimitiveRecursive):
     def __init__(self, n: int, i: int):
+        assert i >= 1 and i <= n
         self.ninputs = n
         self.i = i
 
     def __call__(self, *xs: int) -> int:
         assert len(xs) == self.ninputs
-        assert self.i >= 1 and self.i <= len(xs)
-        ii = self.i-1
-        return xs[ii]
+        return xs[self.i - 1]
 
     def __str__(self) -> str:
-        if self.ninputs == 1:
-            return "(x1 -> x1)"
-        return f"(x1,...,x{self.ninputs} -> x{self.i})"
+        return f"P^{self.ninputs}_{self.i}"
+
+
+"""
+Alternative definition uses Zero function and builts all constant functions with Zero, Successor, and Proj
+"""
+Zero = Constant(1, 0)
+ZeroNoArgs = Constant(0, 0)
+
+
+"""
+Primitive Recursive Operators: Composition and Ro
+"""
+
 
 class Composition(PrimitiveRecursive):
     def __init__(self, g: PrimitiveRecursive, h: PrimitiveRecursive):
@@ -56,12 +72,11 @@ class Composition(PrimitiveRecursive):
         self.noutputs = g.noutputs
 
     def __call__(self, *xs: int) -> int:
-        if type(self.h) == Tuple:
-            return self.g(*self.h(*xs))
-        return self.g(self.h(*xs))
+        return self.g(*self.h(*xs)) if type(self.h) == Tuple else self.g(self.h(*xs))
 
     def __str__(self) -> str:
         return f"({self.g} . {self.h})"
+
 
 class Ro(PrimitiveRecursive):
     def __init__(self, g: PrimitiveRecursive, h: PrimitiveRecursive):
@@ -71,16 +86,21 @@ class Ro(PrimitiveRecursive):
         self.ninputs = g.ninputs + 1
 
     def __call__(self, *xs: int) -> int:
-        f = self
-        x0 = xs[0]
-        if x0 == ZeroNoArgs()():
-            return self.g(*xs[1:])
-        return self.h(*[x0-1, f(*[x0-1, *xs[1:]]), *xs[1:]])
+        return (
+            self.g(*xs[1:])
+            if xs[0] == 0
+            else self.h(*[xs[0] - 1, self(*[xs[0] - 1, *xs[1:]]), *xs[1:]])
+        )
 
     def __str__(self) -> str:
-        return f"(ro({self.g},{self.h}))"
+        return f"\u03c1({self.g}, {self.h})"
+
 
 class Tuple(PrimitiveRecursive):
+    """
+    Primitive recursive function that returns multiple values
+    """
+
     def __init__(self, *fs: PrimitiveRecursive):
         for f in fs:
             assert f.ninputs == fs[0].ninputs
@@ -92,95 +112,144 @@ class Tuple(PrimitiveRecursive):
         return tuple([f(*xs) for f in self.fs])
 
     def __str__(self) -> str:
-        args_str = f"x1,...,x{self.ninputs}"
-        return f"{args_str} -> ({self.fs[0]}({args_str}),...,{self.fs[-1]}({args_str}))"
+        outputs_str = ", ".join([f"{f}" for f in self.fs])
+        return f"({outputs_str})"
 
-def Constant(n: int, k: int) -> PrimitiveRecursive:
-    if n == 0:
-        if k == 0:
-            return ZeroNoArgs()
-        return Composition(Successor(), Constant(0, k-1))
-    if k == 0:
-        return Composition(Zero(), Proj(n, 1))
-    return Composition(Successor(), Constant(n, k-1))
 
-Sum = Ro(Proj(1,1), Composition(Successor(), Proj(3,2)))
-assert Sum(3,5) == 8
+Sum = Ro(Proj(1, 1), Composition(Successor(), Proj(3, 2)))
+assert Sum(3, 5) == 8
 
-Pred = Ro(ZeroNoArgs(), Proj(2,1))
+Pred = Ro(Constant(0, 0), Proj(2, 1))
 assert Pred(0) == 0
 assert Pred(2) == 1
 
-Swap = Tuple(Proj(2,2), Proj(2,1))
-assert Swap(1,2) == (2,1)
+Swap = Tuple(Proj(2, 2), Proj(2, 1))
+assert Swap(1, 2) == (2, 1)
 
-RSub = Ro(Proj(1,1), Composition(Pred, Proj(3,2)))
+RSub = Ro(Proj(1, 1), Composition(Pred, Proj(3, 2)))
 Sub = Composition(RSub, Swap)
-assert Sub(4,9) == 0
-assert Sub(9,4) == 5
+assert Sub(4, 9) == 0
+assert Sub(9, 4) == 5
 
-Mul = Ro(Zero(), Composition(Sum, Tuple(Proj(3,2), Proj(3,3))))
-assert Mul(5,6) == 30
+Mul = Ro(Constant(1, 0), Composition(Sum, Tuple(Proj(3, 2), Proj(3, 3))))
+assert Mul(5, 6) == 30
+assert Mul(5, 0) == 0
 
-If = Ro(Proj(2,2), Proj(4,3))
-assert If(0,8,9) == 9
-assert If(1,8,9) == 8
+If = Ro(Proj(2, 2), Proj(4, 3))
+assert If(0, 8, 9) == 9
+assert If(1, 8, 9) == 8
 
-And = Composition(If, Tuple(Proj(2,1), Proj(2,2), Constant(2,0)))
-assert And(0,0) == 0
-assert And(1,0) == 0
-assert And(0,1) == 0
-assert And(1,1) == 1
+And = Composition(If, Tuple(Proj(2, 1), Proj(2, 2), Constant(2, 0)))
+assert And(0, 0) == 0
+assert And(1, 0) == 0
+assert And(0, 1) == 0
+assert And(1, 1) == 1
 
-Or = Composition(If, Tuple(Proj(2,1), Constant(2,1), Proj(2,2)))
-assert Or(0,0) == 0
-assert Or(0,1) == 1
-assert Or(1,0) == 1
-assert Or(1,1) == 1
+Or = Composition(If, Tuple(Proj(2, 1), Constant(2, 1), Proj(2, 2)))
+assert Or(0, 0) == 0
+assert Or(0, 1) == 1
+assert Or(1, 0) == 1
+assert Or(1, 1) == 1
 
 IsZero = Ro(Constant(0, 1), Constant(2, 0))
 Not = IsZero
 assert IsZero(0) == 1
 assert IsZero(1) == 0
 
-RExp = Ro(Constant(1,1), Composition(Mul, Tuple(Proj(3,2), Proj(3,3))))
+RExp = Ro(Constant(1, 1), Composition(Mul, Tuple(Proj(3, 2), Proj(3, 3))))
 Exp = Composition(RExp, Swap)
-assert Exp(2,6) == 64
+assert Exp(2, 6) == 64
+assert Exp(2, 0) == 1
 
 Leq = Composition(IsZero, Sub)
-assert Leq(9,8) == 0
-assert Leq(8,9) == 1
-assert Leq(8,8) == 1
+assert Leq(9, 8) == 0
+assert Leq(8, 9) == 1
+assert Leq(8, 8) == 1
 
 Geq = Composition(Leq, Swap)
-assert Geq(9,8) == 1
-assert Geq(8,9) == 0
-assert Geq(8,8) == 1
+assert Geq(9, 8) == 1
+assert Geq(8, 9) == 0
+assert Geq(8, 8) == 1
 
 Eq = Composition(And, Tuple(Leq, Geq))
-assert Eq(9,8) == 0
-assert Eq(8,8) == 1
-assert Eq(7,8) == 0
+assert Eq(9, 8) == 0
+assert Eq(8, 8) == 1
+assert Eq(7, 8) == 0
 
-Fac = Ro(Constant(0,1), Composition(Mul, Tuple(Composition(Successor(), Proj(2,1)), Proj(2,2))))
+Less = Composition(And, Tuple(Leq, Composition(Not, Geq)))
+assert Less(9, 8) == 0
+assert Less(8, 9) == 1
+assert Less(8, 8) == 0
+
+Gt = Composition(Less, Swap)
+assert Gt(9, 8) == 1
+assert Gt(8, 9) == 0
+assert Gt(8, 8) == 0
+
+Xor = Composition(And, Tuple(Or, Composition(Not, Eq)))
+assert Xor(0, 0) == 0
+assert Xor(0, 1) == 1
+assert Xor(1, 0) == 1
+assert Xor(1, 1) == 0
+
+Nand = Composition(Not, And)
+assert Nand(0, 0) == 1
+assert Nand(1, 0) == 1
+assert Nand(0, 1) == 1
+assert Nand(1, 1) == 0
+
+Nor = Composition(Not, Or)
+assert Nor(0, 0) == 1
+assert Nor(1, 0) == 0
+assert Nor(0, 1) == 0
+assert Nor(1, 1) == 0
+
+Xnor = Composition(Not, Xor)
+assert Xnor(0, 0) == 1
+assert Xnor(1, 0) == 0
+assert Xnor(0, 1) == 0
+assert Xnor(1, 1) == 1
+
+Fac = Ro(
+    Constant(0, 1),
+    Composition(Mul, Tuple(Composition(Successor(), Proj(2, 1)), Proj(2, 2))),
+)
 assert Fac(6) == 720
 
-Rest = Ro(Zero(), Composition(If, Tuple(
-    Composition(Eq, Tuple(Composition(Pred, Proj(3,3)), Proj(3,2))),
-    Constant(3,0),
-    Composition(Successor(), Proj(3,2))
-)))
+Rest = Ro(
+    Constant(1, 0),
+    Composition(
+        If,
+        Tuple(
+            Composition(Eq, Tuple(Composition(Pred, Proj(3, 3)), Proj(3, 2))),
+            Constant(3, 0),
+            Composition(Successor(), Proj(3, 2)),
+        ),
+    ),
+)
 
 for i in range(0, 30):
-    assert Rest(i,5) == i % 5
+    assert Rest(i, 5) == i % 5
 
-Div = Ro(Zero(), Composition(If, Tuple(
-    Composition(Eq, Tuple(Composition(Pred, Proj(3,3)), Composition(Rest, Tuple(Proj(3,1), Proj(3,3))))),
-    Composition(Successor(), Proj(3,2)),
-    Proj(3,2)
-)))
+Div = Ro(
+    Constant(1, 0),
+    Composition(
+        If,
+        Tuple(
+            Composition(
+                Eq,
+                Tuple(
+                    Composition(Pred, Proj(3, 3)),
+                    Composition(Rest, Tuple(Proj(3, 1), Proj(3, 3))),
+                ),
+            ),
+            Composition(Successor(), Proj(3, 2)),
+            Proj(3, 2),
+        ),
+    ),
+)
 
 for i in range(0, 30):
-    assert Div(i,5) == int(i / 5)
+    assert Div(i, 5) == int(i / 5)
 
-print(Sum)
+print(Sub)
