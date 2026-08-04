7d77390f569bc2dec2fa4431a30b261e7e1e8d28
Paulo Sérgio <pamorimmonico@gmail.com>
2026-08-04 11:27:30 -0300

    comments, remove tuple type

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 class GeneralRecursive:
     ninputs = 1
-    noutputs = 1
 
-    def __call__(self, *args, **kargs):
-        args = args
-        kargs = kargs
+    def __call__(self, *_: int) -> int:
         raise NotImplementedError()
 
 
@@ -18,9 +15,13 @@ Basic Primitive Recursive Functions Set
 
 
 class Successor(PrimitiveRecursive):
-    def __call__(self, x: int) -> int:
-        assert x >= 0
-        return x + 1
+    def __call__(self, *xs: int) -> int:
+        """
+        S(x) = x+1
+        """
+        assert len(xs) == 1
+        assert xs[0] >= 0
+        return xs[0] + 1
 
     def __str__(self) -> str:
         return "S"
@@ -28,12 +29,19 @@ class Successor(PrimitiveRecursive):
 
 class Constant(PrimitiveRecursive):
     def __init__(self, n: int, k: int):
+        """
+        n: n-ary function
+        k: non-negative integer to be returned
+        """
         assert n >= 0
         assert k >= 0
         self.ninputs = n
         self.k = k
 
     def __call__(self, *xs: int) -> int:
+        """
+        C^n_k(x1,...,xn) = k
+        """
         assert len(xs) == self.ninputs
         return self.k
 
@@ -43,11 +51,18 @@ class Constant(PrimitiveRecursive):
 
 class Proj(PrimitiveRecursive):
     def __init__(self, n: int, i: int):
+        """
+        n: n-ary function
+        i: 1-index based argument to be returned
+        """
         assert i >= 1 and i <= n
         self.ninputs = n
         self.i = i
 
     def __call__(self, *xs: int) -> int:
+        """
+        P^n_i(x1,...,xn) = xi
+        """
         assert len(xs) == self.ninputs
         return xs[self.i - 1]
 
@@ -68,28 +83,47 @@ Primitive Recursive Operators: Composition and Rho
 
 
 class Composition(GeneralRecursive):
-    def __init__(self, g: GeneralRecursive, h: GeneralRecursive):
-        assert h.noutputs == g.ninputs
-        self.g = g
+    def __init__(self, h: GeneralRecursive, *gs: GeneralRecursive):
+        """
+        h: n-ary function
+        gs: n k-ary functions
+        """
+        assert len(gs) == h.ninputs
+        for g in gs:
+            assert g.ninputs == gs[0].ninputs
         self.h = h
-        self.ninputs = h.ninputs
-        self.noutputs = g.noutputs
+        self.gs = gs
+        self.ninputs = gs[0].ninputs
 
     def __call__(self, *xs: int) -> int:
-        return self.g(*self.h(*xs)) if type(self.h) == Tuple else self.g(self.h(*xs))
+        """
+        f(x1,...,xk) = h(g1(x1,...,xk),...,gn(x1,...,xk))
+        """
+        return self.h(*(g(*xs) for g in self.gs))
 
     def __str__(self) -> str:
-        return f"({self.g} . {self.h})"
+        if len(self.gs) == 1:
+            return f"({self.h} . {self.gs[0]})"
+        return f"({self.h} . ({', '.join([str(g) for g in self.gs])})"
 
 
 class Rho(GeneralRecursive):
     def __init__(self, g: GeneralRecursive, h: GeneralRecursive):
+        """
+        g: n-ary function
+        h: (n+2)-ary function
+        """
         assert g.ninputs + 2 == h.ninputs
         self.g = g
         self.h = h
         self.ninputs = g.ninputs + 1
 
     def __call__(self, *xs: int) -> int:
+        """
+        f(x,y1,...,yn) =
+            g(y1,...,yn)                      , if x == 0
+            h(x-1,f(x-1,y1,...,yn),y1,...,yn) , otherwise
+        """
         return (
             self.g(*xs[1:])
             if xs[0] == 0
@@ -100,33 +134,24 @@ class Rho(GeneralRecursive):
         return f"\u03c1({self.g}, {self.h})"
 
 
-class Tuple(GeneralRecursive):
-    """
-    General recursive function that returns multiple values
-    """
-
-    def __init__(self, *fs: GeneralRecursive):
-        for f in fs:
-            assert f.ninputs == fs[0].ninputs
-        self.fs = fs
-        self.ninputs = fs[0].ninputs
-        self.noutputs = len(fs)
-
-    def __call__(self, *xs: int) -> tuple[int]:
-        return tuple([f(*xs) for f in self.fs])
-
-    def __str__(self) -> str:
-        outputs_str = ", ".join([f"{f}" for f in self.fs])
-        return f"({outputs_str})"
+"""
+Mi-Recursive Operator
+"""
 
 
 class Mi(GeneralRecursive):
     def __init__(self, f: GeneralRecursive):
-        assert(f.ninputs) >= 1
+        """
+        f: (n+1)-ary function
+        """
+        assert (f.ninputs) >= 1
         self.f = f
         self.ninputs = f.ninputs - 1
 
     def __call__(self, *xs: int) -> int:
+        """
+        mi(f)(x1,...,xn) = max z st. f(z',x1,...,xn) > 0 for z' < z and f(z,x1,...,xn) = 0
+        """
         assert len(xs) == self.ninputs
         z = 0
         while self.f(*[z, *xs]) != 0:
@@ -137,77 +162,107 @@ class Mi(GeneralRecursive):
         return f"\u00b5({self.f})"
 
 
+"""
+Sum(0,y)   = y
+Sum(x+1,y) = x+1+y = S(Sum(x,y))
+"""
 Sum = Rho(Proj(1, 1), Composition(Successor(), Proj(3, 2)))
 assert Sum(3, 5) == 8
 
+"""
+Pred(0)   = 0
+Pred(x+1) = x
+"""
 Pred = Rho(Constant(0, 0), Proj(2, 1))
 assert Pred(0) == 0
 assert Pred(2) == 1
 
-Swap = Tuple(Proj(2, 2), Proj(2, 1))
-assert Swap(1, 2) == (2, 1)
+Swap = Proj(2, 2), Proj(2, 1)
 
+"""
+RSub(0,y)   = y-0     = y
+RSub(x+1,y) = y-(x+1) = y-x-1 = Pred(RSub(x,y))
+"""
 RSub = Rho(Proj(1, 1), Composition(Pred, Proj(3, 2)))
-Sub = Composition(RSub, Swap)
+Sub = Composition(RSub, *Swap)
 assert Sub(4, 9) == 0
 assert Sub(9, 4) == 5
 
-Mul = Rho(Constant(1, 0), Composition(Sum, Tuple(Proj(3, 2), Proj(3, 3))))
+"""
+Mul(0,y)   = 0
+Mul(x+1,y) = (x+1)*y = x*y+y = Sum(Mul(x,y), y)
+"""
+Mul = Rho(Constant(1, 0), Composition(Sum, Proj(3, 2), Proj(3, 3)))
 assert Mul(5, 6) == 30
 assert Mul(5, 0) == 0
 
+"""
+If(0,y,z)   = z
+If(x+1,y,z) = y
+"""
 If = Rho(Proj(2, 2), Proj(4, 3))
 assert If(0, 8, 9) == 9
 assert If(1, 8, 9) == 8
 
-And = Composition(If, Tuple(Proj(2, 1), Proj(2, 2), Constant(2, 0)))
+And = Composition(If, Proj(2, 1), Proj(2, 2), Constant(2, 0))
 assert And(0, 0) == 0
 assert And(1, 0) == 0
 assert And(0, 1) == 0
 assert And(1, 1) == 1
 
-Or = Composition(If, Tuple(Proj(2, 1), Constant(2, 1), Proj(2, 2)))
+Or = Composition(If, Proj(2, 1), Constant(2, 1), Proj(2, 2))
 assert Or(0, 0) == 0
 assert Or(0, 1) == 1
 assert Or(1, 0) == 1
 assert Or(1, 1) == 1
 
+"""
+IsZero(0)   = 1
+IsZero(x+1) = 0
+"""
 IsZero = Rho(Constant(0, 1), Constant(2, 0))
 Not = IsZero
 assert IsZero(0) == 1
 assert IsZero(1) == 0
 
-RExp = Rho(Constant(1, 1), Composition(Mul, Tuple(Proj(3, 2), Proj(3, 3))))
-Exp = Composition(RExp, Swap)
+"""
+RExp(0,y)   = y**0 = 1
+RExp(x+1,y) = y**(x+1) = y**x * y = Mul(RExp(x,y), y)
+"""
+RExp = Rho(Constant(1, 1), Composition(Mul, Proj(3, 2), Proj(3, 3)))
+Exp = Composition(RExp, *Swap)
 assert Exp(2, 6) == 64
 assert Exp(2, 0) == 1
 
+"""
+Leq(x,y) = 0 <=> Sub(x,y) = 0
+"""
 Leq = Composition(IsZero, Sub)
 assert Leq(9, 8) == 0
 assert Leq(8, 9) == 1
 assert Leq(8, 8) == 1
 
-Geq = Composition(Leq, Swap)
+Geq = Composition(Leq, *Swap)
 assert Geq(9, 8) == 1
 assert Geq(8, 9) == 0
 assert Geq(8, 8) == 1
 
-Eq = Composition(And, Tuple(Leq, Geq))
+Eq = Composition(And, Leq, Geq)
 assert Eq(9, 8) == 0
 assert Eq(8, 8) == 1
 assert Eq(7, 8) == 0
 
-Less = Composition(And, Tuple(Leq, Composition(Not, Geq)))
+Less = Composition(And, Leq, Composition(Not, Geq))
 assert Less(9, 8) == 0
 assert Less(8, 9) == 1
 assert Less(8, 8) == 0
 
-Gt = Composition(Less, Swap)
+Gt = Composition(Less, *Swap)
 assert Gt(9, 8) == 1
 assert Gt(8, 9) == 0
 assert Gt(8, 8) == 0
 
-Xor = Composition(And, Tuple(Or, Composition(Not, Eq)))
+Xor = Composition(And, Or, Composition(Not, Eq))
 assert Xor(0, 0) == 0
 assert Xor(0, 1) == 1
 assert Xor(1, 0) == 1
@@ -231,82 +286,94 @@ assert Xnor(1, 0) == 0
 assert Xnor(0, 1) == 0
 assert Xnor(1, 1) == 1
 
+"""
+Fac(0)   = 1
+Fac(x+1) = (x+1)*Fac(x) = Mul(S(x), Fac(x))
+"""
 Fac = Rho(
-    Constant(0, 1),
-    Composition(Mul, Tuple(Composition(Successor(), Proj(2, 1)), Proj(2, 2))),
+    Constant(0, 1), Composition(Mul, Composition(Successor(), Proj(2, 1)), Proj(2, 2))
 )
 assert Fac(6) == 720
 
+"""
+Rest(0,y)   = 0
+Rest(x+1,y) = 
+    0             , if y-1 == Rest(x,y)
+    1 + Rest(x,y) , otherwise 
+"""
 Rest = Rho(
     Constant(1, 0),
     Composition(
         If,
-        Tuple(
-            Composition(Eq, Tuple(Composition(Pred, Proj(3, 3)), Proj(3, 2))),
-            Constant(3, 0),
-            Composition(Successor(), Proj(3, 2)),
-        ),
+        Composition(Eq, Composition(Pred, Proj(3, 3)), Proj(3, 2)),
+        Constant(3, 0),
+        Composition(Successor(), Proj(3, 2)),
     ),
 )
 
 for i in range(0, 30):
     assert Rest(i, 5) == i % 5
 
+"""
+Div(0,y)   = 0
+Div(x+1,y) = 
+    1 + Div(x,y) , if y-1 == Rest(x,y)
+        Div(x,y) , otherwise 
+"""
 Div = Rho(
     Constant(1, 0),
     Composition(
         If,
-        Tuple(
-            Composition(
-                Eq,
-                Tuple(
-                    Composition(Pred, Proj(3, 3)),
-                    Composition(Rest, Tuple(Proj(3, 1), Proj(3, 3))),
-                ),
-            ),
-            Composition(Successor(), Proj(3, 2)),
-            Proj(3, 2),
+        Composition(
+            Eq,
+            Composition(Pred, Proj(3, 3)),
+            Composition(Rest, Proj(3, 1), Proj(3, 3)),
         ),
+        Composition(Successor(), Proj(3, 2)),
+        Proj(3, 2),
     ),
 )
 
 for i in range(0, 30):
     assert Div(i, 5) == int(i / 5)
 
-Mod2 = Composition(Rest, Tuple(Proj(1, 1), Constant(1, 2)))
+Mod2 = Composition(Rest, Proj(1, 1), Constant(1, 2))
 
 for i in range(0, 30):
     assert Mod2(i) == i % 2
 
-Div3 = Composition(Div, Tuple(Proj(1, 1), Constant(1, 3)))
+Div3 = Composition(Div, Proj(1, 1), Constant(1, 3))
 
 for i in range(0, 30):
     assert Div3(i) == int(i / 3)
 
+"""
+Isqrt(x) = max z st. z**2 <= x.
+Incrementa z enquanto (z+1)**2 é <= que a entrada, quando falha, retorna z
+"""
 Isqrt = Mi(
     Composition(
-        Not,
-        Composition(
-            Gt,
-            Tuple(
-                Composition(
-                    Mul,
-                    Tuple(
-                        Composition(Successor(), Proj(2, 1)),
-                        Composition(Successor(), Proj(2, 1)),
-                    ),
-                ),
-                Proj(2, 2),
-            ),
-        ),
+        Leq,
+        Composition(Exp, Composition(Successor(), Proj(2, 1)), Constant(2, 2)),
+        Proj(2, 2),
     )
 )
 
 assert Isqrt(50) == 7
 assert Isqrt(49) == 7
 assert Isqrt(48) == 6
+assert Isqrt(9) == 3
+assert Isqrt(100) == 10
 
+"""
+Não existe z tal que C^1_1 retorne 0.
+NotHalt entra em loop e não para.
+"""
 NotHalt = Mi(Constant(1, 1))
 # NotHalt()
+
+DoubleIt = Composition(Sum, Proj(1, 1), Proj(1, 1))
+assert DoubleIt(0) == 0
+assert DoubleIt(8) == 16
 
 print(Sub)
